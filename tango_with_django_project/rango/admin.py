0d202c0ce31f2572d4f6fd13f4818d0d0c88e96c
eduardo.lupiani <lupianiedu@gmail.com>
2015-02-10 23:55:25 +0100

    End the 5th part of tango with django

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from rango.models import Category, Page
 # Register your models here.
+
+
+# Alternatively we could make use of annotations: @admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)

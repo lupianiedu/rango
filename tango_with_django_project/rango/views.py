b1a37eb7916d31fd2f45fc606eba2080c94dc73a
eduardo.lupiani <lupianiedu@gmail.com>
2015-02-10 22:44:44 +0100

    Add about template

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -9,6 +9,6 @@ def index(request):
     context_dict = {'boldmessage': "I am bold font from the context"}
     return render(request, 'rango/index.html', context_dict)
 
+
 def about(request):
-    return HttpResponse("<p>Rango says here is the about page.</p>"
-                        " <a href='/rango/'>Index</a>")
+    return render(request, 'rango/about.html')
